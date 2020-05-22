2284a5dae541d898bbc5ae042b6aca50e7239414
zeh3 <zeh3@illinois.edu>
2020-05-22 18:59:36 -0500

    added structs and methods

--- include/spotify/song_listens.h
+++ include/spotify/song_listens.h
@@ -6,8 +6,11 @@
 #define SPOTIFY_SONG_LISTENS_H
 
 #include <string>
+#include "nlohmann/json.hpp"
 
 using std::string;
+using std::vector;
+using json = nlohmann::json;
 
 namespace spotify {
 
@@ -26,6 +29,15 @@ struct SongTotalListens {
   Song song;
   long total_milliseconds_listened;
 };
+
+struct ArtistTotalListens {
+  string artist;
+  long total_milliseconds_listened;
+};
+
+vector<SongListen> ParseJson(const vector<json>& songs);
+
+vector<SongTotalListens> SortSongs(const vector<SongListen>& song_listens);
 
 }
 
