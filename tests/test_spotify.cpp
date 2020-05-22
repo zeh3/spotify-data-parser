9ec3a4bf2e9fcae7a897e2d489055f17220b3b76
zeh3 <zeh3@illinois.edu>
2020-05-22 12:51:22 -0500

    tests set up, everything linked

---
+++ tests/test_spotify.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Lou Zeh on 5/22/20.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include "spotify/song_listens.h"
+
+TEST_CASE("sanity check") {
+  REQUIRE(1 == 1);
+}
+
+
+
