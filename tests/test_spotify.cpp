b9a792e992310fbbac2b7d7c399fca98a6ac08c2
zeh3 <zeh3@illinois.edu>
2020-05-25 18:31:10 -0500

    test cases for json parsing

--- tests/test_spotify.cpp
+++ tests/test_spotify.cpp
@@ -7,8 +7,30 @@
 #include <catch2/catch.hpp>
 #include "spotify/song_listens.h"
 
-TEST_CASE("sanity check") {
-  REQUIRE(1 == 1);
+using std::string;
+using std::ifstream;
+using namespace spotify;
+
+TEST_CASE("test parsing", "[ParseJson]") {
+  string example_file_path = "data/ExampleOneDay.json";
+  ifstream json_stream(example_file_path);
+  json j;
+  json_stream >> j;
+  vector<json> json_vector = j.get<std::vector<json> >();
+  vector<SongListen> song_listens = ParseJson(json_vector);
+  
+  Song bleh("bleh", "example artist");
+  string first_time = "1995-02-19 1:15";
+  long first_ms = 1000;
+  SongListen first(bleh, first_ms, first_time);
+  REQUIRE(first == song_listens[0]);
+  
+  Song sorry("I'm sorry", "me");
+  string ninth_time = "1995-02-19 10:15";
+  long ninth_ms = 200;
+  SongListen ninth(sorry, ninth_ms, ninth_time);
+  REQUIRE(ninth == song_listens[9]);
+  
 }
 
 
